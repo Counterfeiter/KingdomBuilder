3186b236bef11bad1a3c6144ef83005dc8e98fe3
Sebastian Foerster <basti.foerster@gmx.de>
2021-03-07 22:15:37 +0100

    - move test to unit test - but some player three mountain counting fails  -> maybe the author counting was wrong
    - add the first 4 simple quadrants as files
    - read quadrants and build a random board (TODO: add random rotation)

--- board.py
+++ board.py
@@ -1,55 +1,9 @@
-import math
+import random
 from enum import Enum, unique
-
-board_env = """
-. . W . . . . . . . . . W . . . . . . .
- . M W W . . . . . . . W . . C . . . . .
-. M . . W . . . T . . W . . . . . . . .
- M M . W . . . . . . . W . . . . . . . .
-. . M . W W . . . . W W . . . . . T . .
- . . . . . W W W W W . . C . . . . . . .
-. T . . . . . . . . . . . . . . . M M M
- . . W . . C . . . . . . . . . M M M M M
-. . W W . . . . M . . W . W . . . . M M
- . . . . . . . M M . . W W . . . . . M .
-. . . . . . . . . . . . . . . . . . . .
- M . . . . . . . . . . . . T . . . . . .
-M M . C . . . . . . . . . . . . . . . .
- M . . . . . . . . . M M . . . . . . . .
-. . . W . . . . . . . . . . . . . W W W
- . . . . W . T . . . . . T . . . W . . .
-. . . W . . . . . . . . . . . . W . C .
- C . . . . . . . . . . . . . . . W . . M
-. . . . . M . . . . . . . . . . W . . .
- . . . . . M M . . . . . . . . . W W . .
-"""
-
-board_settlements = """
-3 3 . . . . 4 . 4 . . 2 . . 4 . . 4 . 4
- 3 . . . . 1 1 . . 4 2 . . 3 . 4 4 . . 4
-3 . 2 2 . 1 1 1 . 3 2 . 4 3 . 1 4 . 4 .
- . . . . 2 2 . . . 2 2 . 3 . 1 1 1 . . .
-. 4 . . . . 2 2 2 2 . . 3 . 1 4 . . . .
- . . . . . . . . . . . 3 . 1 . . 2 2 2 2
-. . 1 1 1 1 . . 2 . . 4 . . . 2 2 . . .
- 4 . . 4 . . 4 4 . . . . . . 2 . . . . .
-. 4 . . . . . 4 . . . . . . . 2 2 2 . .
- . . . . . . . . . . . . . . . . . 2 . .
-. . . 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2 . 1
- . 3 3 . . . . . . . . 4 . . 2 . 2 4 1 .
-. . . . . 4 . 4 . . . . . 1 2 4 2 1 1 .
- . . . 1 . 4 . . . . . . 1 2 . . 2 1 . .
-. . . . 1 1 4 1 1 . . . 1 2 . . 2 . . .
- . . 1 1 . 1 . . 1 1 1 1 . . . 2 . . 4 .
-. 1 1 . . 3 3 . . . . . . . . 2 . 4 . 3
- . 1 3 3 3 . 3 . 4 . 4 . 4 . . 2 . 1 1 .
-4 3 3 4 . . 4 3 . . . . . . . 2 . . . .
- . . . 4 . . . 3 . . 4 4 . 4 . 2 . . . .
-"""
+import os
 
 @unique
 class BOARDSECTIONS(Enum):
-    NONE = 0
     ORACLE = 1
     FARM = 2
     OASIS = 3
@@ -65,7 +19,6 @@ class BOARDSECTIONS(Enum):
 
 @unique
 class TERRAIN(Enum):
-    NONE = ' '
     GRASS = 'G'
     FLOWER = 'B' #german "blume"
     FOREST = 'F'
@@ -109,11 +62,39 @@ class CARDRULES(Enum):
 
 class Board:
 
-    def __init__(self):
+    def __init__(self, folderpath):
         self.max_players = 5
         self.playerlist = [str(x) for x in range(1, self.max_players + 1)]
-        self.board_env = [row.split() for row in board_env.strip().split("\n")]
-        self.board_settlements = [row.split() for row in board_settlements.strip().split("\n")]
+        self.board_settlements = [ [' ']*20 for i in range(20)]
+        self.load_quadrants(folderpath)
+        self.board_env = self.joinquadrants()
+
+    def load_quadrants(self, foldername):
+        self.env_quadrants = {"quadrants" : []}
+        quadrant_names = [x.name for x in BOARDSECTIONS.list()]
+        for filename in os.listdir(foldername):
+            qua_name = os.path.basename(filename)
+            if qua_name in quadrant_names:
+                with open(os.path.join(foldername,filename), 'r') as file:
+                    raw_board = file.read()
+                    self.env_quadrants["quadrants"].append(qua_name)
+                    self.env_quadrants[qua_name] = [row.split() for row in raw_board.strip().split("\n")]
+
+    def joinquadrants(self, quadrant_name_list : list = []):
+        if len(quadrant_name_list) != 4:
+            quadrant_name_list = random.sample(self.env_quadrants["quadrants"], 4)
+        #TODO: random rotation
+        qua = [self.env_quadrants[x] for x in quadrant_name_list]
+        board = []
+        for row in range(10):
+            board.append(qua[0][row] + qua[1][row])
+        for row in range(10):
+            board.append(qua[2][row] + qua[3][row])
+        return board
+
+    @property
+    def env(self):
+        return self.board_env
 
     def is_env(self, row, col, env):
         return self.board_env[row][col] == env
@@ -136,9 +117,9 @@ class Board:
         str_out = ""
         for i, row in enumerate(board):
             if i % 2 == 1:
-                str_out += " "
+                str_out += "  "
 
-            str_out += " " + " ".join(row) + "\n"
+            str_out += "   " + "   ".join(row) + "\n"
         return str_out[:-1]
 
     #source is https://codegolf.stackexchange.com/questions/44485/score-a-game-of-kingdom-builder
@@ -152,63 +133,75 @@ class Board:
         return {(r,c) for r,c in neighbour_set if 20>r>-1 and 20>c>-1}
 
     @staticmethod
-    def is_neighbour(board, row, col, object1, object2):
-        return board[row][col] == object1 and object2 in {board[r][c] for r,c in Board.neighbours(row, col)}
+    def is_neighbour(board1, board2, row, col, object1, object2):
+        #FIXME: water points should be compared with env map, because players could be on the water with the harbor
+        if board2 == None:
+            board2 = board1
+        return board1[row][col] == object1 and object2 in {board1[r][c] for r,c in Board.neighbours(row, col)}
 
 class Rules:
 
-    def __init__(self, board : Board):
+    def __init__(self, board : Board, cards: list = []):
         self.board = board
-        self.rule_set = CARDRULES.list()
-        self.rules = [CARDRULES.CASTLE, self.rule_set[1], self.rule_set[5], self.rule_set[7]]
-        #self.rules = [self.rule_set[1]]
+        if len(cards) == 3:
+            self.rules = [CARDRULES.CASTLE] + cards
+        else:
+            self.randomcards()
 
-    def score(self, player_list):
+    def randomcards(self):
+        self.rule_set = CARDRULES.list()[1:]
+        self.rules = [CARDRULES.CASTLE]
+        self.rules += random.sample(self.rule_set, 3)
+        return self.rules
+
+    def score(self, player_list : list):
         player_score = [0.0] * len(player_list)
 
         quadrant_houses_per_player = [ [0]*len(player_list) for i in range(4)]
+
+        score_per_rule = [ [0] * len(CARDRULES) for i in range(len(player_list))]
 
         board_copy_score_7 = self.board.resulting_board()
         board_copy = self.board.resulting_board()
 
         quadrants = self.board.quadrants()
 
-        for i, player in enumerate(player_list):
-
+        for i, player_class in enumerate(player_list):
+            player = str(player_class)
             ### row and column based counting
             max_sattlements_row = 0
             max_sattlements_group = 0
             for row in range(20):
                 max_sattlements_row_current = 0
                 if CARDRULES.DICOVERES in self.rules:
-                    player_score[i] += (player in board_copy[row])
+                    score_per_rule[i][CARDRULES.DICOVERES.value] += (player in board_copy[row])
                 for col in range(20):
                     max_sattlements_row_current += (board_copy[row][col] == player)
                     if CARDRULES.CASTLE in self.rules:
-                        player_score[i] += 3 * Board.is_neighbour(board_copy, row, col, SPECIALLOCATION.CASTLE.value, player)
+                        score_per_rule[i][CARDRULES.CASTLE.value] += 3 * Board.is_neighbour(board_copy, None, row, col, SPECIALLOCATION.CASTLE.value, player)
                     if CARDRULES.WORKER in self.rules:
-                        player_score[i] += Board.is_neighbour(board_copy, row, col, player, SPECIALLOCATION.CASTLE.value)
-                        player_score[i] += Board.is_neighbour(board_copy, row, col, player, SPECIALLOCATION.TOWNFULL)
-                        player_score[i] += Board.is_neighbour(board_copy, row, col, player, SPECIALLOCATION.TOWNHALF)
-                        player_score[i] += Board.is_neighbour(board_copy, row, col, player, SPECIALLOCATION.TOWNEMPTY)
+                        score_per_rule[i][CARDRULES.WORKER.value] += Board.is_neighbour(board_copy, None, row, col, player, SPECIALLOCATION.CASTLE.value)
+                        score_per_rule[i][CARDRULES.WORKER.value] += Board.is_neighbour(board_copy, None, row, col, player, SPECIALLOCATION.TOWNFULL)
+                        score_per_rule[i][CARDRULES.WORKER.value] += Board.is_neighbour(board_copy, None, row, col, player, SPECIALLOCATION.TOWNHALF)
+                        score_per_rule[i][CARDRULES.WORKER.value] += Board.is_neighbour(board_copy, None, row, col, player, SPECIALLOCATION.TOWNEMPTY)
                     if CARDRULES.FISHERMEN in self.rules:
-                        player_score[i] += (Board.is_neighbour(board_copy, row, col, player, SPECIALLOCATION.WATER.value) and not self.board.is_env(row,col, 'W'))
+                        score_per_rule[i][CARDRULES.FISHERMEN.value] += (Board.is_neighbour(board_copy, board.env, row, col, player, SPECIALLOCATION.WATER.value) and not self.board.is_env(row,col, 'W'))
                     if CARDRULES.MINERS in self.rules:
-                        player_score[i] += Board.is_neighbour(board_copy, row, col, player, SPECIALLOCATION.MOUNTAIN.value)
+                        score_per_rule[i][CARDRULES.MINERS.value] += Board.is_neighbour(board_copy, board.env, row, col, player, SPECIALLOCATION.MOUNTAIN.value)
                     is_single_group, num_of_sattlements = self.rule_7_score(player, board_copy_score_7, row, col, 0)
                     if num_of_sattlements > max_sattlements_group:
                         max_sattlements_group = num_of_sattlements
                     if CARDRULES.HERMITS in self.rules:
-                        player_score[i] += is_single_group
+                        score_per_rule[i][CARDRULES.HERMITS.value] += is_single_group
 
                 if max_sattlements_row_current > max_sattlements_row:
                     max_sattlements_row =  max_sattlements_row_current
             
             
             if CARDRULES.KNIGHTS in self.rules:
-                player_score[i] += max_sattlements_row * 2
+                score_per_rule[i][CARDRULES.KNIGHTS.value] += max_sattlements_row * 2
             if CARDRULES.CITIZENS in self.rules:
-                player_score[i] += max_sattlements_group // 2
+                score_per_rule[i][CARDRULES.CITIZENS.value] += max_sattlements_group // 2
 
             if CARDRULES.LOARDS in self.rules or CARDRULES.FARMERS in self.rules:
                 ## quadrant based counting
@@ -218,8 +211,9 @@ class Rules:
                             quadrant_houses_per_player[i][index_qua] += (quadrant[row][col] == player)
 
                 if CARDRULES.FARMERS in self.rules:
-                    player_score[i] += min(quadrant_houses_per_player[i]) * 3
+                    score_per_rule[i][CARDRULES.FARMERS.value] += min(quadrant_houses_per_player[i]) * 3
 
+        #quadrant based on sattlements from other players
         if CARDRULES.LOARDS in self.rules:
             tran_ply_qua = [ list(e) for e in zip(*quadrant_houses_per_player)]
             for qua in tran_ply_qua:
@@ -230,8 +224,14 @@ class Rules:
                         continue
                     for player, houses in enumerate(qua):
                         if max_houses == houses:
-                            player_score[player] += gold
+                            score_per_rule[player][CARDRULES.LOARDS.value] += gold
                             qua[player] = 0
+
+        for i in range(len(player_list)):
+            player_list[i].score = sum(score_per_rule[i])
+            player_score[i] = sum(score_per_rule[i])
+
+        self.player_score_per_rule = score_per_rule
 
         return player_score
 
@@ -245,13 +245,47 @@ class Rules:
                 _, counter = self.rule_7_score(player, board_modify, *neighbour, counter)
 
         return is_char, counter
+
+class Player:
+    def __init__(self, index:str):
+        self.takecard()
+        self.player_index = index
+        self.starter = False # player starts the round
+        self._score = 0
+
+    def takecard(self):
+        self.current_card = random.choice(TERRAIN.list())
+        return self.current_card
+
+    def setStarter(self):
+        self.starter = True
+
+    def isStarter(self):
+        return self.starter
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score):
+        self._score = score
+
+    def __str__(self):
+        return self.player_index
+
         
 
-board = Board()
+board = Board("quadrants")
 
 rules = Rules(board)
 
+print(rules.randomcards())
+
+players = [ Player(ind) for ind in ["1", "2", "3", "4"] ]
+
 print(board)
 
+print(rules.score(players))
 
-print(rules.score(["1", "2", "3", "4"]))
+print(players[0].takecard())
